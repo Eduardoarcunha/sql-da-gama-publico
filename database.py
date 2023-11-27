810496cddfa1be5bf368ee81135e2e9ed728b0cc
Eduardo <eduardoarcunha12@gmail.com>
2023-11-27 17:58:38 -0300

    add env

--- database.py
+++ database.py
@@ -5,8 +5,8 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv(override=True)
-# SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{os.getenv('username')}:{os.getenv('password')}@localhost:3306/rottentomatoes"
-SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:banana@10.20.3.34:3306/rottentomatoes"
+SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://{os.getenv('username')}:{os.getenv('password')}@{os.getenv('server_ip')}:3306/rottentomatoes"
+# SQLALCHEMY_DATABASE_URL = f"mysql+pymysql://root:banana@10.20.3.34:3306/rottentomatoes"
 
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
